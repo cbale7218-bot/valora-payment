633818e8caabf540354f63a27fb39b9e7ce2de54
valorapayment <cbale7218@gmail.com>
2026-08-24 21:56:43 +0400

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,6 @@ const CONFIG_FALLBACK = {
   bank: "Название банка",
   accountHolder: "Nombre del titular",
   accountNumber: "0000000000",
-  cedula: "ES00 0000 0000 0000 0000 0000",
   swift: "XXXXXX",
   reference: "Referencia del pago",
   amount: "0.00",
